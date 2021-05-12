eecdd596ca6d1bbbe25fcc5e6fe55902f2b9f3fa
steplogic <kojogyaase@gmail.com>
2021-05-12 12:53:45 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import Container from "./components/Container";
+import SVGHeader from './SVGHeader';
+ReactDOM.render(
+  <React.StrictMode>
+  <SVGHeader/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
